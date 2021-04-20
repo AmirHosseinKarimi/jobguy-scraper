4f999aa96ee1b34901bfeafd147f115d32f27d07
Amir hossein Karimi <amir.hossein7464@gmail.com>
2021-04-20 19:48:47 +0430

    Add get-reviews script

---
+++ get-reviews.js
@@ -0,0 +1,44 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+fs.readdir("../jobguy/companies/", async (err, companies) => {
+  var errors = [];
+  var rawData, data, reviewJson;
+
+  for (const companyName of companies) {
+    path = `../jobguy/companies/${companyName}/`;
+
+    console.log(`Company: ${companyName}`);
+
+    rawData = fs.readFileSync(`${path}review.json`);
+    data = JSON.parse(rawData);
+
+    if (data.data.length) {
+      for (const review of data.data) {
+        try {
+          var response = await fetch(
+            "https://api.jobguy.ir/public/company_review/" + review.id
+          );
+
+          if (!response.ok) {
+            throw response.status;
+          }
+
+          console.info(`${review.id}: ✅`);
+
+          reviewJson = await response.json();
+
+          fs.writeFileSync(
+            `../jobguy/reviews/${review.id}.json`,
+            JSON.stringify(reviewJson)
+          );
+        } catch (error) {
+          errors.push(review.id);
+          console.error(`${review.id}: ❌`);
+        }
+      }
+    }
+  }
+
+  fs.writeFileSync(`errors.json`, JSON.stringify(errors));
+});
